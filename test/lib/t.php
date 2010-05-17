a6541acb6cacf3b68ae878c2b561c6de3927b5bb
tell-k <tell-k@224e63c0-5457-0410-989b-907d9cf58824>
2010-05-17 00:00:56 +0000

    * dircrotry構成を変更
    git-svn-id: http://svn.openpear.org/Services_Buzzurl/trunk@1844 224e63c0-5457-0410-989b-907d9cf58824

--- test/lib/t.php
+++ test/lib/t.php
@@ -1,3 +1,3 @@
 <?php
-ini_set('include_path', realpath(dirname(__FILE__) . '/../../src'));
+ini_set('include_path', realpath(dirname(__FILE__) . '/../../'));
 require_once dirname(__FILE__) . '/lime/lime.php';
